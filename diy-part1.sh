c3ea5da0cb164204abf142cf3f91352e0b61604f
tangtao <tangtao@gmail.com>
2022-07-17 14:56:05 +0800

    update Config file

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,12 +10,6 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '$a src-git danshui https://github.com/281677160/openwrt-package.git;master' feeds.conf.default
 
-# 清除旧版argon主题并拉取最新版
-rm -rf package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-# Remove UnblockNeteaseMusicGo upx commands
-sed -i "/upx/d" package/lean/UnblockNeteaseMusicGo/Makefile || true
